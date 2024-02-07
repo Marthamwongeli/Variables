1c46f14bb9abbb8c30be5c804cb1491520405e68
Marthamwongeli <marthamutuaa@gmail.com>
2024-02-07 22:34:32 +0300

    variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+//fun main(){
+//        var numPlanets: Byte = 8
+//        numPlanets = 127
+//
+//        var numWorldCountries: Short = 195
+//        numWorldCountries = 32767
+//
+//        var africaPopulation: Int =1216000000
+//        africaPopulation = 2147483647
+//
+//        var worldPopulation: Long = 78880000000000000
+//
+//        var weight: Float=32.473F
+//
+//        var weight: Double = 45.699987
+//
+//        var myInitial:Char = 'R'
+//
+//        var citizenship: String = "Kenyan"
+//
+//        var lightsOn = true
+//
+//        var minor = false
+//
+//        var names = arrayOf("martha","mutua" "yvonne,mwende")
+
+
+}
+
+fun main(){
+    var names: String ="JamesOlenKatai"
+    var age: Byte = 25
+    var number: Int = 0-719265993
+    var weight : Byte = 54
+    var kenyanCitizen : Boolean = true
+
+
+}
